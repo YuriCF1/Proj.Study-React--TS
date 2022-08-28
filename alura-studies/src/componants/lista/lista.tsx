864384c49516ef0df958bf34934dd6d6e719f3f5
Yuri <yurifdev@gmail.com>
2022-08-28 12:21:22 -0300

    Separting the responsability of the creation of items inside the li list

--- alura-studies/src/componants/lista/lista.tsx
+++ alura-studies/src/componants/lista/lista.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React from "react"; //Instrutor disse que no function component, ele não precisa ser usado
+import Item from "./item";
 
-// Style 
+// Style
 // import './style.scss'
-import style from './lista.module.scss'
+import style from "./lista.module.scss";
 
 function Lista() {
   // Criando array de tarefas para renderizar
@@ -16,24 +17,43 @@ function Lista() {
       tempo: "01:00:00",
     },
     {
-        tarefa: "TypeScript",
-        tempo: "03:00:00",
-    }
+      tarefa: "TypeScript",
+      tempo: "03:00:00",
+    },
   ];
 
   // A função não precisa do render como na classe. Já que usando classe, tem que extender do React.Component
-  return ( //O JSX sempre espera um html
+  return (
+    //O JSX sempre espera um html
     <aside className={style.listaTarefas}>
       <h2>Estudos do dia</h2>
-      <ul> 
-        {tarefas.map((item, index) => ( //Tomar cuidado com as chaves, pois elas definem um escopo. Os PARENTESES são o retorno 
-// O react, no map, precisa saber qual item do array corresponde ao dom. Então precisa-se de um chave para likar, chmada de Key. O ideal é colocar um ID, em vez desse índice
-            <li key={index} className={style.item}> 
-                <h3>{item.tarefa}</h3>
-                <span>{item.tempo}</span>
-            </li>
+      <ul>
+        {tarefas.map(
+          (
+            item,
+            index //Tomar cuidado com as chaves, pois elas definem um escopo. Os PARENTESES são o retorno
+          ) => (
 
-        ))}
+            // O react, no map, precisa saber qual item do array corresponde ao dom. Então precisa-se de um chave para likar, chmada de Key. O ideal é colocar um ID, em vez desse índice
+            // <li key={index} className={style.item}>
+            //     <h3>{item.tarefa}</h3>
+            //     <span>{item.tempo}</span>
+            // </li>
+
+            // Primeira forma para importar
+            // <Item 
+            //   tarefa={item.tarefa}
+            //   tempo={item.tempo}
+            // />
+
+            // Segunda forma para importar, pois a tarefa é um objeto
+            <Item 
+              key={index}
+              {...item} // Usando cada elemento como Prop na function Item
+              // Desustruturando. Cuidado se o objeto tiver muita informação, pode dá problema
+            />
+          )
+        )}
         {/* <li>
           <h3>React</h3>
           <span>02:00:00</span>
