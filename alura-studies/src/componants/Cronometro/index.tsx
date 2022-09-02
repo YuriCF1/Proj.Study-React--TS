93a9dc9fe1ae8f2adf48d78c1d384040093d3c4f
Yuri <yurifdev@gmail.com>
2022-09-02 20:34:12 -0300

    Refactoring the code

--- alura-studies/src/componants/Cronometro/index.tsx
+++ alura-studies/src/componants/Cronometro/index.tsx
@@ -1,13 +1,13 @@
 import Botao from "../Botao";
 import Relogio from "./Relogio";
 
-import style from './cronometro.module.scss'
+import style from "./cronometro.module.scss";
 
 export default function Cronometro() {
   return (
-    <div className= {style.cronometro}>
-      <p className= {style.titulo}>Escolha um card e inicie o cronômetro</p>
-      <div className= {style.relogioWrapper}>
+    <div className={style.cronometro}>
+      <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
+      <div className={style.relogioWrapper}>
         <Relogio />
       </div>
       <Botao>Começar</Botao>
