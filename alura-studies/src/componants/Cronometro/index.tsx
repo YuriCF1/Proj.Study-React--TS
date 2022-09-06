d0c400a4a904911afc09520cf2b1a6967c1f3078
Yuri <yurifdev@gmail.com>
2022-09-05 22:04:02 -0300

    Creating the fuction to get the time data

--- alura-studies/src/componants/Cronometro/index.tsx
+++ alura-studies/src/componants/Cronometro/index.tsx
@@ -1,9 +1,11 @@
 import Botao from "../Botao";
 import Relogio from "./Relogio";
+import { tempoParaSegundos } from "../../common/utils/date"; //Ele tem chave pois não é suporte default
 
 import style from "./cronometro.module.scss";
 
 export default function Cronometro() {
+  console.log('conversao: ', tempoParaSegundos('01:01:01'));
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
